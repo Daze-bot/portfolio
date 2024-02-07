2c7c5f8e34444e88c9a41eb5678adc5a6889616d
Code4Daze <jevon.fillette@gmail.com>
2024-02-06 22:28:15 -0500

    Add edgewood to projects

--- src/database/projectLibrary.js
+++ src/database/projectLibrary.js
@@ -5,6 +5,7 @@ import Blog from '../imgs/projectScreenshots/blog.png';
 import Battleship from '../imgs/projectScreenshots/battleship.jpg';
 import MembersOnly from '../imgs/projectScreenshots/members-only.png';
 import Todo from '../imgs/projectScreenshots/to-do.jpg';
+import Edgewood from '../imgs/projectScreenshots/edgewood.png';
 import PhotoFinderRec from '../video/photo-finder-rec.gif';
 import ShoppingRec from '../video/shopping-rec.gif';
 import MemoryRec from '../video/memory-rec.gif';
@@ -12,6 +13,7 @@ import BattleshipRec from '../video/battleship-rec.gif';
 import TodoRec from '../video/todo-rec.gif';
 import BlogRec from '../video/blog-rec.gif';
 import MembersOnlyRec from '../video/members-only-rec.gif';
+import EdgewoodRec from '../video/edgewood-rec.gif';
 
 const projectLibrary = [
   {
@@ -40,7 +42,7 @@ const projectLibrary = [
   },
   {
     name: "Members Only",
-    description: "Online chat room built on the back-end using Node, Express, and MongoDB.  Users first have to become a member before they can access the chat",
+    description: "Online chat room built on the back-end using Node, Express, and MongoDB.  Users first have to become a member before they can access the chat.",
     screenshot: MembersOnly,
     githubLink: "https://github.com/Daze-bot/members-only",
     projectLink: "https://daze-members-only.fly.dev/",
@@ -55,6 +57,14 @@ const projectLibrary = [
     video: MemoryRec,
   },
   {
+    name: 'Edgewood Cafe',
+    description: "Fully responsive app that was built for the owner of Edgewood Cafe.  This is the official restaurant website and was created in order to establish an online presence to better serve customers.",
+    screenshot: Edgewood,
+    githubLink: "https://github.com/Daze-bot/edgewood-cafe",
+    projectLink: "https://edgewoodcafeyardley.com/",
+    video: EdgewoodRec,
+  },
+  {
     name: 'To-do List',
     description: "A comprehensive to-do list to assist with daily/weekly/monthly tasks.  The user can create multiple projects to store different tasks, with a variety of sorting and viewing options.",
     screenshot: Todo,
@@ -64,7 +74,7 @@ const projectLibrary = [
   },
   {
     name: 'Battleship ',
-    description: 'A classic game of Battleship developed using Test Driven Development.  Play against a competitive and difficult AI.',
+    description: 'The classic game of Battleship developed using Test Driven Development.  Play against a competitive and difficult AI.',
     screenshot: Battleship,
     githubLink: "https://github.com/Daze-bot/battleship",
     projectLink: "https://daze-bot.github.io/battleship/",
